d0e57c08d0d70a644c57cc8fc4a0080f0eef6071
The5thAxiom <samridh.anand.paatni@gmail.com>
2023-12-13 11:03:18 +0530

    added text entry to set current frame

--- main.py
+++ main.py
@@ -15,16 +15,14 @@ def ocv2_image_to_tk(image):
 
 def get_frame(video: cv2.VideoCapture, frame_no: int):
     video.set(cv2.CAP_PROP_POS_FRAMES, frame_no)
-    ret, frame = video.read()
-    if not ret:
-        return False, None
-    return True, ocv2_image_to_tk(frame)
+    return video.read()
 
-current_video_path = '../datasets/video_split_fullhd/DJI_0804_0001_30m_1.mp4'
+current_video_path = '../datasets/video_split_fullhd/DJI_0915_0031_20m.mp4'
 current_video = cv2.VideoCapture(current_video_path)
 h = int(current_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 w = int(current_video.get(cv2.CAP_PROP_FRAME_WIDTH))
 current_frame_no: int = 0
+current_frame = None
 num_frames = int(current_video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 window = tk.Tk()
@@ -34,22 +32,17 @@ canvas = tk.Canvas(window, width=w, height=h)
 canvas_image = canvas.create_image(0, 0)
 
 def show_current_frame():
-    global canvas
-    global canvas_image
-    global current_frame_no
-    global current_video
-    no_error, frame = get_frame(current_video, current_frame_no)
-    if no_error:
-        canvas.image = frame
-        canvas.itemconfig(canvas_image, image=frame)
-    else:
-        print('error hogyi bhaiya')
+    global canvas, canvas_image
+    global current_frame
+
+    frame = ocv2_image_to_tk(current_frame)
+    canvas.image = frame
+    canvas.itemconfig(canvas_image, image=frame)
 
 def manage_frame_controls():
-    global current_frame_no
-    global current_video
-    global next_button, prev_button
-    global frame_slider
+    global current_video, current_frame_no
+    global next_button, prev_button, frame_slider, frame_input
+
     if current_frame_no == 0:
         prev_button['state'] = "disabled"
     else:
@@ -60,37 +53,65 @@ def manage_frame_controls():
         next_button['state'] = "active"
 
     frame_slider.set(current_frame_no)
+    frame_input.delete(0, tk.END)
+    frame_input.insert(0, current_frame_no)
 
 def increment_frame():
     global current_frame_no, num_frames
     if current_frame_no < num_frames:
         current_frame_no += 1
-        manage_frame_controls()
-        show_current_frame()
+        set_current_frame()
 
 def decrement_frame():
     global current_frame_no
     if current_frame_no > 0:
         current_frame_no -= 1
-        manage_frame_controls()
-        show_current_frame()
+        set_current_frame()
 
-def set_frame(event):
+def set_frame_from_slider(event):
     global current_frame_no
     global frame_slider
     current_frame_no = int(frame_slider.get())
+    set_current_frame()
+
+def set_frame_from_entry(*args):
+    global current_frame_no
+    global frame_input_value, frame_input_confirm
+    current_frame_no = int(frame_input_value.get())
+    set_current_frame()
+
+def manage_frame_input_confirm(*args):
+    global current_frame_no
+    global frame_input_value, frame_input_confirm
+    if frame_input_value.get().isdigit() and int(frame_input_value.get()) != current_frame_no:
+        frame_input_confirm['state'] = 'active'
+    else:
+        frame_input_confirm['state'] = 'disabled'
+
+def set_current_frame():
+    global current_frame
     manage_frame_controls()
+    no_error, current_frame = get_frame(current_video, current_frame_no)
+    if not no_error:
+        return
     show_current_frame()
+
 
 next_button = tk.Button(window, text="Next Frame", command=increment_frame)
 prev_button = tk.Button(window, text="Previous Frame", command=decrement_frame)
-frame_slider = tk.Scale(window, from_=0, to=num_frames, orient='horizontal', command=set_frame, length=w/2)
+frame_slider = tk.Scale(window, from_=0, to=num_frames, orient='horizontal', command=set_frame_from_slider, length=w/2)
+frame_input_value = tk.StringVar()
+frame_input_value.trace("w", manage_frame_input_confirm)
+frame_input = tk.Entry(window, textvariable=frame_input_value)
+frame_input.bind('<Return>', set_frame_from_entry)
+frame_input_confirm = tk.Button(window, text="Set Frame", command=set_frame_from_entry)
 
-manage_frame_controls()
-show_current_frame()
+set_current_frame()
 
 next_button.pack()
 frame_slider.pack()
+frame_input.pack()
+frame_input_confirm.pack()
 prev_button.pack()
 canvas.pack()
 
